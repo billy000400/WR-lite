ad636513fc2d4e8f249908c9b500c83de8085b32
Billy Li <Billy000400@gmail.com>
2021-07-30 18:21:45 -0500

    deb

--- roofit/testFit.C
+++ roofit/testFit.C
@@ -11,10 +11,24 @@ using namespace RooFit;
 
 void testFit()
 {
-  // importing ntuples
+  // importing ntuples into RooDataSet
   RooRealVar WR_RecoMass("WR_RecoMass", "WR_RecoMass", 0, 3000);
   RooRealVar N_RecoMass("N_RecoMass", "N_RecoMass", 0, 2000);
   RooDataSet ds("ds", "ds",
                 RooArgSet(WR_RecoMass, N_RecoMass),
                 ImportFromFile("test.root","analysis/WR_N_Mass_1"));
+
+  // Draw ntuples
+  RooPlot *frame1 = WR_RecoMass.frame(Title("WR Reco Mass:Number:Reco Mass (GeV)"));
+  ds.plotOn(frame1);
+
+  RooPlot *frame2 = N_RecoMass.frame(Title("N Reco Mass:Number:Reco Mass (GeV)"));
+  ds.plotOn(frame2);
+
+  TCanvas *c = new TCanvas("Test Fit", "Test Fit", 1000, 800);
+  c->Divide(1,2);
+  c->cd(1);
+  frame1->Draw();
+  c->cd(2);
+  frame2->Draw();
 }
