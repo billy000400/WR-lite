c4ae7148285faa5a7a986a35d35dbc2781fa5281
Billy Li <Billy000400@gmail.com>
2021-08-02 12:02:54 -0500

    test

--- roofit/testFit.C
+++ roofit/testFit.C
@@ -23,7 +23,7 @@ void testFit()
 
   // preparing the signal distribution
   RooRealVar m0("m0","m0",1000, 900, 1200);
-  RooRealVar sigma("sigma","sigma", 100, -500, 500);
+  RooRealVar sigma("sigma","sigma", 130, 90, 200);
   RooRealVar alpha("alpha", "alpha", 0.1, -0.25, 0.25);
   RooRealVar n("n","n", 0.1, -10, 10);
   RooCBShape cb("signal", "cb signal",
