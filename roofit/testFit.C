8a62ed649c97d6b54b87de430163e90155d92fac
Billy Li <Billy000400@gmail.com>
2021-08-02 08:50:43 -0500

    test

--- roofit/testFit.C
+++ roofit/testFit.C
@@ -18,15 +18,23 @@ void testFit()
                 RooArgSet(WR_RecoMass, N_RecoMass),
                 ImportFromFile("/data/cmszfs1/user/li000400/CMSSW_10_4_0_patch1/src/ExoAnalysis/WR_lite/roofit/test.root","analysis/WR_N_Mass_1"));
 
-  for (int i(0); i<60; i++)
-  {
-    ds.get(i)->Print("V");
-  }
+  // preparing the signal distribution
+  RooRealVar m0("m0","m0",1000, 900, 1100);
+  RooRealVar sigma("sigma","sigma", 200, 0, 500);
+  RooRealVar alpha("alpha", "alpha", 1, 0, 500);
+  RooRealVar n("n","n", 1, 0, 500);
+  RooCBShape cb("signal", "cb signal",
+                WR_RecoMass,
+                m0, sigma, alpha, n);
+
+  // fit distribution to data
+  cb.fitTo(ds);
 
 
   // Draw ntuples
   RooPlot *frame1 = WR_RecoMass.frame(Title("WR Reco Mass"));
   ds.plotOn(frame1, Binning(32));
+  cb.plotOn(frame1);
 
   RooPlot *frame2 = N_RecoMass.frame(Title("N Reco Mass"));
   ds.plotOn(frame2, Binning(32));
