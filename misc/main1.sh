7eb27abec47162f283ea1a0130ede19faaf6b070
Billy Li <li000400@umn.edu>
2021-08-10 23:53:54 -0500

    change main1.sh to ml mode

--- misc/main1.sh
+++ misc/main1.sh
@@ -1,8 +1,16 @@
+# @Author: Billy Li <billyli>
+# @Date:   08-05-2021
+# @Email:  li000400@umn.edu
+# @Last modified by:   billyli
+# @Last modified time: 08-10-2021
+
+
+
 addressTwo="/local/cms/user/li000400/CMSSW_10_4_0_patch1/src/ExoAnalysis/WR_lite/WR_N_mass/"
 address="/hdfs/cms/user/krohn045/WR_SignalSamples/WR800_N400/"
 for i in {1..28}
 do
-	cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR800_N400/out_WR800N400_${i}.root  isSignal=True > /dev/null &
+	cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR800_N400/out_WR800N400_${i}.root isSignal=True genTrainData=True  trainFile=ml.txt > /dev/null &
 	if (( $i%7==0 )); then
 		wait
 	fi
@@ -11,7 +19,7 @@ done
 address="/hdfs/cms/user/krohn045/WR_SignalSamples/WR800_N600/"
 for i in {1..28}
 do
-	cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR800_N600/out_WR800N600_${i}.root  isSignal=True > /dev/null &
+	cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR800_N600/out_WR800N600_${i}.root  isSignal=True genTrainData=True  trainFile=ml.txt > /dev/null &
 	if (( $i%7==0 )); then
 		wait
 	fi
@@ -20,7 +28,7 @@ done
 address="/hdfs/cms/user/krohn045/WR_SignalSamples/WR800_N700/"
 for i in {1..28}
 do
-	cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR800_N700/out_WR800N700_${i}.root  isSignal=True > /dev/null &
+	cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR800_N700/out_WR800N700_${i}.root  isSignal=True genTrainData=True  trainFile=ml.txt > /dev/null &
 	if (( $i%7==0 )); then
 		wait
 	fi
@@ -32,7 +40,7 @@ done
 address="/hdfs/cms/user/krohn045/WR_SignalSamples/WR1000_N200/"
 for i in {1..28}
 do
-	cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR1000_N200/out_WR1000N200_${i}.root  isSignal=True > /dev/null &
+	cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR1000_N200/out_WR1000N200_${i}.root  isSignal=True genTrainData=True  trainFile=ml.txt > /dev/null &
 	if (( $i%7==0 )); then
 		wait
 	fi
@@ -42,7 +50,7 @@ address="/hdfs/cms/user/krohn045/WR_SignalSamples/WR1000_N900/"
 for i in {1..28}
 do
 	if (($i!=19)); then
-		cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR1000_N900/out_WR1000N900_${i}.root  isSignal=True > /dev/null &
+		cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR1000_N900/out_WR1000N900_${i}.root  isSignal=True genTrainData=True  trainFile=ml.txt > /dev/null &
 		if (( $i%7==0 )); then
 			wait
 		fi
@@ -53,7 +61,7 @@ done
 address="/hdfs/cms/user/krohn045/WR_SignalSamples/WR1000_N400/"
 for i in {1..28}
 do
-	cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR1000_N400/out_WR1000N400_${i}.root  isSignal=True > /dev/null &
+	cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR1000_N400/out_WR1000N400_${i}.root  isSignal=True genTrainData=True  trainFile=ml.txt > /dev/null &
 	if (( $i%7==0 )); then
 		wait
 	fi
@@ -63,7 +71,7 @@ done
 address="/hdfs/cms/user/krohn045/WR_SignalSamples/WR1000_N600/"
 for i in {1..28}
 do
-	cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR1000_N600/out_WR1000N600_${i}.root  isSignal=True > /dev/null &
+	cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR1000_N600/out_WR1000N600_${i}.root  isSignal=True genTrainData=True  trainFile=ml.txt > /dev/null &
 	if (( $i%7==0 )); then
 		wait
 	fi
@@ -72,7 +80,7 @@ done
 address="/hdfs/cms/user/krohn045/WR_SignalSamples/WR1000_N800/"
 for i in {1..28}
 do
-	cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR1000_N800/out_WR1000N800_${i}.root  isSignal=True > /dev/null &
+	cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR1000_N800/out_WR1000N800_${i}.root  isSignal=True genTrainData=True  trainFile=ml.txt > /dev/null &
 	if (( $i%7==0 )); then
 		wait
 	fi
@@ -83,7 +91,7 @@ done
 address="/hdfs/cms/user/krohn045/WR_SignalSamples/WR800_N200/"
 for i in {1..6}
 do
-	cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR800_N200/out_WR800N200_${i}.root  isSignal=True > /dev/null &
+	cmsRun python/cfg.py inputFiles=file:${address}MINIAOD_${i}.root  outputFile=${addressTwo}WR800_N200/out_WR800N200_${i}.root  isSignal=True genTrainData=True  trainFile=ml.txt > /dev/null &
 	if (( $i%7==0 )); then
 		wait
 	fi
