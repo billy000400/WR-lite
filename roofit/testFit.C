2d791b04866bf9075b912e1b9985cee3d3aa73e9
Billy Li <li000400@umn.edu>
2021-08-18 00:55:04 -0500

    debug

--- roofit/testFit.C
+++ roofit/testFit.C
@@ -30,11 +30,11 @@ void testFit()
                 RooArgSet(*WR_RecoMass, *N_RecoMass_Match, *N_RecoMass_NN),
                 ImportFromFile("../WR2000_N1900/out_WR2000N1900_4.root","analysis/WR_N_RecoMass"));
 
-  RooDataSet ds1("ds2", "ds2",
+  RooDataSet ds2("ds2", "ds2",
                 RooArgSet(*WR_RecoMass, *N_RecoMass_Match, *N_RecoMass_NN),
                 ImportFromFile("../WR2000_N1900/out_WR2000N1900_4.root","analysis/WR_N_RecoMass"));
 
-  RooPlot *frame1 = WR_RecoMass.frame(Title("WR Reco Mass"));
+  RooPlot *frame1 = WR_RecoMass->frame(Title("WR Reco Mass"));
   ds1.plotOn(frame1, Binning(32));
 
   // preparing the signal distribution
@@ -44,10 +44,10 @@ void testFit()
   RooRealVar n("n","n", 0.1, -10, 10);
 
   RooGaussian gauss("gauss", "gaussian pdf",
-                  WR_RecoMass,
+                  *WR_RecoMass,
                   m0, sigma);
   RooCBShape cb("signal", "cb signal",
-                WR_RecoMass,
+                *WR_RecoMass,
                 m0, sigma, alpha, n);
   cb.plotOn(frame1);
 
@@ -63,7 +63,7 @@ void testFit()
   // Draw ntuples
 
 
-  RooPlot *frame2 = N_RecoMass.frame(Title("N Reco Mass"));
+  RooPlot *frame2 = N_RecoMass_Match.frame(Title("N Reco Mass"));
   ds1.plotOn(frame2, Binning(32));
 
   TCanvas *c = new TCanvas("Test Fit", "Test Fit", 1000, 800);
