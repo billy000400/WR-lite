44e45810e31b828cdacbd9b534391deeb0e58be1
Billy Li <li000400@umn.edu>
2021-08-18 00:40:24 -0500

    the last try

--- roofit/testFit_DoubleCB.C
+++ roofit/testFit_DoubleCB.C
@@ -40,23 +40,31 @@ void testFit_DoubleCB()
   ds1.plotOn(frame3, Binning(128));
 
   // preparing the signal distribution
-  RooAddPdf* WR_pdf = DoubleCB(WR_RecoMass);
+  RooRealVar* rrv_mean_CB = new RooRealVar("rrv_mean_CB", "rrv_mean_CB", 2000, 1000, 3000);
+  RooRealVar* rrv_sigma_CB = new RooRealVar("rrv_sigma_CB", "rrv_sigma_CB", 100, 50, 300);
+  RooRealVar* rrv_tail_CB_I = new RooRealVar("rrv_tail_CB_I", "rrv_tail_CB_I", 2,0., 40);
+  RooRealVar* rrv_tail_CB_II = new RooRealVar("rrv_tail_CB_II", "rrv_tail_CB_II", -2., -40., 0.);
+
+  RooRealVar* rrv_normalization_CB_I = new RooRealVar("rrv_normalization_CB_I", "rrv_normalization_CB_I", 2, 0., 400);
+  RooRealVar* rrv_frac_CB = new RooRealVar("rrv_frac_CB", "rrv_frac_CB", 0.5);
+
+
+  RooCBShape* Crystal_Ball_I = new RooCBShape("CrystalBall_I", "CrystalBall_I",
+                                              *WR_RecoMass,
+                                              *rrv_mean_CB,*rrv_sigma_CB,*rrv_tail_CB_I,*rrv_normalization_CB_I);
+
+  RooCBShape* Crystal_Ball_II = new RooCBShape("CrystalBall_II", "CrystalBall_II",
+                                              *rrv_x,
+                                              *rrv_mean_CB,*rrv_sigma_CB,*rrv_tail_CB_II,*rrv_normalization_CB_I);
+
+  RooAddPdf* model_pdf = new RooAddPdf("model_pdf", "model_pdf",
+                                      RooArgList(*Crystal_Ball_I,*Crystal_Ball_II),
+                                      RooArgList(*rrv_frac_CB));
 
   // fit distribution to data
   WR_pdf->fitTo(ds1);
 
-  RooArgList coefList = WR_pdf->coefList();
-  RooAbsArg* coef = coefList.at(0);
-
-  std::set<std::string> attributes =coef->attributes();
-  std::set<std::string >::iterator it ;
-  std::cout << "The element of set s are : \n";
-  for (it = attributes.begin() ; it != attributes.end() ; it++ )
-  {
-      std::cout << *it<<" ";
-  }
-  std::cout << std::endl;
-  std::count << coef["Constant"] << std::endl;
+  std::cout << "The value of rrv_frac_CB is" << rrv_frac_CB << std::endl;
 
   // Draw ntuples
   WR_pdf->plotOn(frame1);
