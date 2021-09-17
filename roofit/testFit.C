109ee80b8286d71cfa4e7cb403989e302ba6b15a
Billy Li <li000400@umn.edu>
2021-09-16 20:14:27 -0500

    debug

--- roofit/testFit.C
+++ roofit/testFit.C
@@ -244,6 +244,7 @@ double NEvtInRange(RooDataSet& ds, double min, double max)
   Int_t numEntries=ds.numEntries();
   for (Int_t i=0; i<numEntries; i++){
     auto data = ds.get(i)->getRealValue("V");
+    std::cout << data << std::endl;
     if ((data>min)&&(data<max)) num++;
   }
   return num;
