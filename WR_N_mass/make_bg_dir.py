96323b5e1cf87f565c838141e943f7edbfff4eff
Billy Li <li000400@umn.edu>
2022-04-28 22:47:30 -0500

    add new make dir

---
+++ WR_N_mass/make_bg_dir.py
@@ -0,0 +1,35 @@
+# @Author: Billy Li <billyli>
+# @Date:   04-28-2022
+# @Email:  li000400@umn.edu
+# @Last modified by:   billyli
+# @Last modified time: 04-28-2022
+
+
+
+from pathlib import Path
+
+# k_dir = Path("/hdfs/cms/user/evans908/wrSkims/")
+# dir_names = k_dir.glob("*")
+# dir_names = [dir.stem for dir in dir_names]
+
+dir_names = ["DYJetsToLL_M-50_HT-100to200",
+            "DYJetsToLL_M-50_HT-1200to2500",
+            "DYJetsToLL_M-50_HT-200to400",
+            "DYJetsToLL_M-50_HT-2500toInf",
+            "DYJetsToLL_M-50_HT-600to800",
+            "DYJetsToLL_M-50_HT-800to1200",
+            "TTTo2L2Nu",
+            "TTToSemilepton_TuneCUETP8M2_ttHtranche3"]
+
+cwd = Path.cwd()
+making_dirs = [cwd.joinpath(dir.split) for dir in dir_names]
+
+for i in range(5):
+	print(f"making directory {making_dirs[i]}")
+
+print("...")
+
+for dir in making_dirs:
+	dir.mkdir()
+
+print("All directories are made")
