1e07a1dad1e06cd50bda568373246c1c3a906e3d
Haoyang Li <li000400@umn.edu>
2022-05-03 16:33:34 -0500

    dev

--- roofit/testFit_DY.C
+++ roofit/testFit_DY.C
@@ -13,14 +13,23 @@ RooAddPdf* DoubleCB(RooRealVar* rrv_x);
 
 void testFit_DY()
 {
-  // importing ntuples into RooDataSet
+  // Preparing RooRealVars
   RooRealVar* lljjRecoMass = new RooRealVar("lljjRecoMass", "lljjRecoMass", 0, 3000);
   RooRealVar* ljjRecoMass_Res = new RooRealVar("ljjRecoMass_Res", "ljjRecoMass_Res", 0, 2500);
   RooRealVar* ljjRecoMass_SpRes = new RooRealVar("ljjRecoMass_SpRes", "ljjRecoMass_SpRes", 0, 2500);
 
+  // importing ntuples into RooDataSet
+  std::string prefix = "../";
+  std::vector<std::string> folders;
+  folders.push_back((prefix+"DYJetsToLL_M-50_HT-100to200/").c_str());
+  folders.push_back((prefix+"DYJetsToLL_M-50_HT-100to200/").c_str());
+  folders.push_back((prefix+"DYJetsToLL_M-50_HT-100to200/").c_str());
+  folders.push_back((prefix+"DYJetsToLL_M-50_HT-100to200/").c_str());
+  folders.push_back((prefix+"DYJetsToLL_M-50_HT-100to200/").c_str());
   RooDataSet ds1("ds1", "ds1",
                 RooArgSet(*lljjRecoMass, *ljjRecoMass_Res, *ljjRecoMass_SpRes),
-                ImportFromFile("dyTest.root","analysis/bgRecoMass"));
+                ImportFromFile(prefix"dyTest.root","analysis/bgRecoMass"));
+
 
   RooPlot *frame1 = lljjRecoMass->frame(Title("DY lljj Reco Mass (top 2 pT lepton)"));
   ds1.plotOn(frame1, Binning(128));
@@ -29,7 +38,7 @@ void testFit_DY()
   RooPlot *frame3 = ljjRecoMass_SpRes->frame(Title("DY ljj Mass, Reco by SuperResolved NN"));
   ds1.plotOn(frame3, Binning(128));
 
-  // preparing the signal distribution
+  // preparing the background distribution
   RooAddPdf* WR_pdf = DoubleCB(lljjRecoMass);
 
   // fit distribution to data
@@ -49,31 +58,4 @@ void testFit_DY()
   frame2->Draw();
   c->cd(3);
   frame3->Draw();
-}
-
-RooAddPdf* DoubleCB(RooRealVar* rrv_x)
-{
-  RooRealVar* rrv_mean_CB = new RooRealVar("rrv_mean_CB", "rrv_mean_CB", 400, 200, 600);
-  RooRealVar* rrv_sigma_CB = new RooRealVar("rrv_sigma_CB", "rrv_sigma_CB", 200, 100, 300);
-  RooRealVar* rrv_tail_CB_I = new RooRealVar("rrv_tail_CB_I", "rrv_tail_CB_I", 2,0., 40);
-  RooRealVar* rrv_tail_CB_II = new RooRealVar("rrv_tail_CB_II", "rrv_tail_CB_II", -2., -40., 0.);
-
-  RooRealVar* rrv_normalization_CB_I = new RooRealVar("rrv_normalization_CB_I", "rrv_normalization_CB_I", 2, 0., 400);
-  RooRealVar* rrv_frac_CB = new RooRealVar("rrv_frac_CB", "rrv_frac_CB", 0.5);
-
-
-  RooCBShape* Crystal_Ball_I = new RooCBShape("CrystalBall_I", "CrystalBall_I",
-                                              *rrv_x,
-                                              *rrv_mean_CB,*rrv_sigma_CB,*rrv_tail_CB_I,*rrv_normalization_CB_I);
-
-  RooCBShape* Crystal_Ball_II = new RooCBShape("CrystalBall_II", "CrystalBall_II",
-                                              *rrv_x,
-                                              *rrv_mean_CB,*rrv_sigma_CB,*rrv_tail_CB_II,*rrv_normalization_CB_I);
-
-  RooAddPdf* model_pdf = new RooAddPdf("model_pdf", "model_pdf",
-                                      RooArgList(*Crystal_Ball_I,*Crystal_Ball_II),
-                                      RooArgList(*rrv_frac_CB));
-
-
-  return model_pdf;
 }
