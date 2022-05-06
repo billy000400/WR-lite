cc5cc93c1da0a8dad8defcff241fe4ba61ac00fc
Billy Li <li000400@umn.edu>
2022-05-05 22:56:28 -0500

    change dataset range

--- roofit/testFit.C
+++ roofit/testFit.C
@@ -56,8 +56,8 @@ void testFit(std::string filePath)
 
   //// importing TNtuples from root files
   std::string prefix = "../analysis/allEvents/";
-  RooRealVar* WR_RecoMass_ee = new RooRealVar("WR_RecoMass_ee", "WR_RecoMass_ee", WRGenMean*0.45, WRGenMean*1.45);
-  RooRealVar* WR_RecoMass_mumu = new RooRealVar("WR_RecoMass_mumu", "WR_RecoMass_mumu", WRGenMean*0.45, WRGenMean*1.45);
+  RooRealVar* WR_RecoMass_ee = new RooRealVar("WR_RecoMass_ee", "WR_RecoMass_ee", WRGenMean*0.5, WRGenMean*1.4);
+  RooRealVar* WR_RecoMass_mumu = new RooRealVar("WR_RecoMass_mumu", "WR_RecoMass_mumu", WRGenMean*0.5, WRGenMean*1.4);
   RooDataSet ds_WR_RecoMass_ee("ds1", "ds1",
                 RooArgSet(*WR_RecoMass_ee),
                 ImportFromFile((prefix+filePath).c_str(), "analysis/WR_RecoMass_ee"));
--- roofit/testFit_ExpmCB.C
+++ roofit/testFit_ExpmCB.C
@@ -41,8 +41,8 @@ void testFit_ExpmCB(std::string filePath)
 
 
   std::string prefix = "../analysis/allEvents/";
-  RooRealVar* WR_RecoMass_ee = new RooRealVar("WR_RecoMass_ee", "WR_RecoMass_ee", WRGenMean*0.5, WRGenMean*1.45);
-  RooRealVar* WR_RecoMass_mumu = new RooRealVar("WR_RecoMass_mumu", "WR_RecoMass_mumu", WRGenMean*0.5, WRGenMean*1.45);
+  RooRealVar* WR_RecoMass_ee = new RooRealVar("WR_RecoMass_ee", "WR_RecoMass_ee", WRGenMean*0.5, WRGenMean*1.4);
+  RooRealVar* WR_RecoMass_mumu = new RooRealVar("WR_RecoMass_mumu", "WR_RecoMass_mumu", WRGenMean*0.5, WRGenMean*1.4);
   RooDataSet ds_WR_RecoMass_ee("ds1", "ds1",
                 RooArgSet(*WR_RecoMass_ee),
                 ImportFromFile((prefix+filePath).c_str(), "analysis/WR_RecoMass_ee"));
@@ -95,7 +95,7 @@ void testFit_ExpmCB(std::string filePath)
   double chi2_mumu_ExpmCB = mumuFrame_ExpmCB->chiSquare();
   std::cout << "chi2_ee_ExpmCB: " << chi2_ee_ExpmCB <<std::endl;
   std::cout << "chi2_mumu_ExpmCB: " << chi2_mumu_ExpmCB <<std::endl;
-  
+
 
   //// plot the errors of the fitted functions
   //// This needs to be done after the pulls was calculated
