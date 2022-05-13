9784a0e6bd43122fa00be874e243c8bc54b307e0
Billy Li <li000400@umn.edu>
2022-05-13 11:38:47 -0500

    dev

--- roofit/testFit_bg.C
+++ roofit/testFit_bg.C
@@ -63,31 +63,43 @@ void testFit_bg()
 
 
   // declare model
-  RooRealVar *c = new RooRealVar("c", "c", -5e-2, -1e-1, -1e-7);
-  RooExponential *model = new RooExponential("exponential bg", "exponential bg", *mumujjMass_bg, *c);
+  RooRealVar *c1 = new RooRealVar("c1", "c1", -5e-2, -1e-1, -1e-7);
+  RooRealVar *c2 = new RooRealVar("c2", "c2", -5e-2, -1e-1, -1e-7);
+  RooRealVar *c3 = new RooRealVar("c3", "c3", -5e-2, -1e-1, -1e-7);
+  RooExponential *model1 = new RooExponential("exponential bg DY", "exponential bg", *mumujjMass_DY, *c1);
+  RooExponential *model2 = new RooExponential("exponential bg ttbar", "exponential bg", *mumujjMass_ttbar, *c2);
+  RooExponential *model3 = new RooExponential("exponential bg DY+ttbar", "exponential bg", *mumujjMass_bg, *c3);
 
   // fit model
-  RooFitResult *r = model->fitTo(ds_bg_mumujj, Save(), SumW2Error(kTRUE), Range(500, 3000));
+  RooFitResult *r1 = model1->fitTo(ds_DY_mumujj, Save(), SumW2Error(kTRUE), Range(500, 3000));
+  RooFitResult *r2 = model2->fitTo(ds_ttbar_mumujj, Save(), SumW2Error(kTRUE), Range(500, 3000));
+  RooFitResult *r3 = model3->fitTo(ds_bg_mumujj, Save(), SumW2Error(kTRUE), Range(500, 3000));
 
   // prepare frames for plotting
-  RooPlot *frame1 = mumujjMass_bg->frame(Title("DY+TTbar mumujj Reco Mass"));
+  RooPlot *frame1 = mumujjMass_DY->frame(Title("DY mumujj Reco Mass"));
+  RooPlot *frame2 = mumujjMass_ttbar->frame(Title("TTbar mumujj Reco Mass"));
+  RooPlot *frame3 = mumujjMass_bg->frame(Title("DY+TTbar mumujj Reco Mass"));
 
 
 
   //// Plot on frames
   // plot data on frames
-  ds_bg_mumujj.plotOn(frame1, Binning(128));
+  ds_DY_mumujj.plotOn(frame1, Binning(20));
+  ds_ttbar_mumujj.plotOn(frame2, Binning(20));
+  ds_bg_mumujj.plotOn(frame3, Binning(20));
 
   // plot fitted pdfs on frames
-  model->plotOn(frame1);
+  model1->plotOn(frame1);
+  model2->plotOn(frame2);
+  model3->plotOn(frame3);
 
 
   TCanvas *canvas = new TCanvas("Test Fit", "Test Fit", 1500, 500);
-  canvas->Divide(1,1);
+  canvas->Divide(3,1);
   canvas->cd(1);
   frame1->Draw();
-  // canvas->cd(2);
-  // frame2->Draw();
-  // canvas->cd(3);
-  // frame3->Draw();
+  canvas->cd(2);
+  frame2->Draw();
+  canvas->cd(3);
+  frame3->Draw();
 }
