243a910ec9c653fdb81404e90d55412ab696f4dd
Billy Li <li000400@umn.edu>
2022-05-14 13:38:37 -0500

    change bin nunber

--- roofit/testFit_logBg.C
+++ roofit/testFit_logBg.C
@@ -84,9 +84,9 @@ void testFit_logBg()
 
   //// Plot on frames
   // plot data on frames
-  ds_DY_mumujj.plotOn(frame1, Binning(128), SumW2Error(kTRUE));
-  ds_ttbar_mumujj.plotOn(frame2, Binning(128), SumW2Error(kTRUE));
-  ds_bg_mumujj.plotOn(frame3, Binning(128), SumW2Error(kTRUE));
+  ds_DY_mumujj.plotOn(frame1, Binning(20), SumW2Error(kTRUE));
+  ds_ttbar_mumujj.plotOn(frame2, Binning(20), SumW2Error(kTRUE));
+  ds_bg_mumujj.plotOn(frame3, Binning(20), SumW2Error(kTRUE));
 
   // plot fitted pdfs on frames
   // model1->plotOn(frame1);
