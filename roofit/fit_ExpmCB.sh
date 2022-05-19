1e60fda056ef03af49719d1dbc81474bd3519fe4
Billy Li <li000400@umn.edu>
2022-05-19 10:39:52 -0500

    dev

--- roofit/fit_ExpmCB.sh
+++ roofit/fit_ExpmCB.sh
@@ -3,5 +3,4 @@
 # @Email:  li000400@umn.edu
 # @Last modified by:   billyli
 # @Last modified time: 05-19-2022
-root -l RooExpmCB.cxx "fit_ExpmCB.C(\"fullWR800N200.root\")"
-.q
+root -l -b RooExpmCB.cxx "fit_ExpmCB.C(\"fullWR800N200.root\")" -q
