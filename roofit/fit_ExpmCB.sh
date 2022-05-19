b5f84d196e02a5640b5ca51bd3a27b449aef48b7
Billy Li <li000400@umn.edu>
2022-05-19 12:50:18 -0500

    dev

--- roofit/fit_ExpmCB.sh
+++ roofit/fit_ExpmCB.sh
@@ -4,10 +4,9 @@
 # @Email:  li000400@umn.edu
 # @Last modified by:   billyli
 # @Last modified time: 05-19-2022
-# root -l -b RooExpmCB.cxx "fit_ExpmCB.C(\"fullWR800N200.root\")" -q
 
 fullFileDir="../analysis/allEvents"
 for i in $fullFileDir/fullWR*.root; do
   filename=$(basename $i)
-  echo $filename
+  root -l -b RooExpmCB.cxx "fit_ExpmCB.C(\"$filename\")" -q
 done
