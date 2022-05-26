7450aa068316ba510d72b583c52189c71f7c3bf2
Billy Li <li000400@umn.edu>
2022-05-26 12:43:36 -0500

    added a shell script to fit all mass pairs to DSCB

---
+++ roofit/fit_DSCB.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# @Author: Billy Li <billyli>
+# @Date:   05-19-2022
+# @Email:  li000400@umn.edu
+# @Last modified by:   billyli
+# @Last modified time: 05-26-2022
+
+fullFileDir="../analysis/allEvents"
+for i in $fullFileDir/fullWR*.root; do
+  filename=$(basename $i)
+  root -l -b RooDSCBShape.cxx "fit_DSCB.C(\"$filename\")" -q
+done
