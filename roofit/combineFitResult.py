1cf96cb1df2252b3824c0940e0ba0d1db0d1628f
Billy Li <li000400@umn.edu>
2022-05-30 12:43:08 -0500

    debug

--- roofit/combineFitResult.py
+++ roofit/combineFitResult.py
@@ -38,7 +38,7 @@ for dist in dists:
                     varName = current_line.split(None,1)
                     varNames.append(varName)
                     current_line_num += 1
-                    print(current_line_num)
+                    print(current_line_num, lines[current_line_num], len(lines[current_line_num]))
 
                 for varName in varNames:
                     data[varName] = []
