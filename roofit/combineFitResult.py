af0f7cbe67c98f8abcad0b4f41cffa8a4074f69c
Billy Li <li000400@umn.edu>
2022-05-30 12:37:25 -0500

    dev to adapt any number of parameters

--- roofit/combineFitResult.py
+++ roofit/combineFitResult.py
@@ -15,17 +15,11 @@ recoMethods = ['ee', 'mumu']
 for dist in dists:
     print(f"[INFO]: Combining fit results for {dist}")
     for recoMethod in recoMethods:
-        # dict for df
-        data = {"WR":[], "N":[],\
-                "alpha":[], "alpha_err":[],\
-                "n":[], "n_err":[],\
-                "beta":[], "beta_err":[],\
-                "m":[], "m_err":[],\
-                "mean":[], "mean_err":[],\
-                "sigma":[], "sigma_err":[],\
-                "chi2":[]
-                }
+        data = {} # dict for making pd df
+        catchVarName = False
+        varNames = []
 
+        # list all result files
         result_dir = Path.cwd().joinpath("results_"+dist+"_"+recoMethod)
         files = result_dir.glob('*.txt')
         for file in files:
@@ -33,78 +27,46 @@ for dist in dists:
             lines = f.readlines()
             f.close()
 
+            # catch all variable names at the first run
+            if not catchVarName:
+                data["WR"] = []
+                data["N"] = []
+
+                current_line_num=9 # RooFit record starts
+                while (len(lines[current_line_num]) != 0):
+                    current_line = lines[current_line_num]
+                    varName = current_line.split(None,1)
+                    varNames.append(varName)
+                    current_line_num += 1
+
+                for varName in varNames:
+                    data[varName] = []
+                    data[f"{varName}_err"] = []
+
+                data["chi2"] = []
+                catchVarName = True
+
             # extract WR mass
-            if "WR:" in lines[0]:
-                WRMass = int(lines[0][3:])
-            elif "WR" in lines[0]:
-                WRMass = int(lines[0][2:])
-            data['WR'].append(WRMass)
+            WR_line = lines[0]
+            WR_text = WR_line.split(":",2)
+            data['WR'].append(float(WR_text))
 
             # extract N mass
-            if "N:" in lines[1]:
-                NMass = int(lines[1][2:])
-            elif "N" in lines[1]:
-                NMass = int(lines[1][1:])
-            data['N'].append(NMass)
+            N_line = lines[1]
+            N_text = N_line.split(":",2)
+            data['N'].append(float(N_text))
 
-            # extract alpha
-            line_alpha = lines[9]
-            try:
-                # parameter is positive
-                name_alpha, alpha_init, alpha_result = line_alpha.split("    ")
-            except:
-                # parameter is negative (will use a space to mark ""-"")
-                name_alpha, alpha_init, alpha_result = line_alpha.split("   ")
-            alpha_final, alpha_errCorr = alpha_result.split(" +/- ")
-            alpha_error, _ = alpha_errCorr.split("  ")
-            data['alpha'].append(float(alpha_final))
-            data['alpha_err'].append(float(alpha_error))
-
-            # extract n
-            line_n = lines[13]
-            name_n, n_values = line_n.split('jj    ')
-            n_init, n_result = n_values.split("    ")
-            n_final, n_errCorr = n_result.split(" +/- ")
-            n_error, _ = n_errCorr.split("  ")
-            data['n'].append(float(n_final))
-            data['n_err'].append(float(n_error))
-
-            # extract beta
-            line_beta = lines[10]
-            name_beta, beta_init, beta_result = line_beta.split("    ")
-            beta_final, beta_errCorr = beta_result.split(" +/- ")
-            beta_error, _ = beta_errCorr.split("  ")
-            data['beta'].append(float(beta_final))
-            data['beta_err'].append(float(beta_error))
-
-            # extract m
-            line_m = lines[11]
-            name_m, m_values = line_m.split('jj    ')
-            m_init, m_result = m_values.split("    ")
-            m_final, m_errCorr = m_result.split(" +/- ")
-            m_error, _ = m_errCorr.split("  ")
-            data['m'].append(float(m_final))
-            data['m_err'].append(float(m_error))
-
-            # extract mean
-            line_mean = lines[12]
-            name_mean, mean_init, mean_result = line_mean.split("    ")
-            mean_final, mean_errCorr = mean_result.split(" +/- ")
-            mean_error, _ = mean_errCorr.split("  ")
-            data['mean'].append(float(mean_final))
-            data['mean_err'].append(float(mean_error))
-
-            # extract sigma
-            line_sigma = lines[14]
-            name_sigma, sigma_init, sigma_result = line_sigma.split("    ")
-            sigma_final, sigma_errCorr = sigma_result.split(" +/- ")
-            sigma_error, _ = sigma_errCorr.split("  ")
-            data['sigma'].append(float(sigma_final))
-            data['sigma_err'].append(float(sigma_error))
+            # loop through variable names to extract final values and errs
+            current_line_num=9 # RooFit record starts
+            for varName in varNames:
+                current_line = lines[current_line_num]
+                _,_,final,_,err,_=current_line.split()
+                data[varName].append(final)
+                data[f"{varName}_err"].append(err)
 
             # extract chi2
-            line_chi2 = lines[16]
-            _, chi2 = line_chi2.split(": ")
+            line_chi2 = lines[-1]
+            _, chi2 = line_chi2.split()
             data["chi2"].append(float(chi2))
 
         df = pd.DataFrame.from_dict(data)
