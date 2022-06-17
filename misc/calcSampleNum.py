bac0b8d058ff7367cc9721934550ea5b78dc304a
Billy Li <li000400@umn.edu>
2022-06-16 22:25:27 -0500

    calculate sample number for mumujj and eejj separately

--- misc/calcSampleNum.py
+++ misc/calcSampleNum.py
@@ -13,10 +13,12 @@ ROOT.ROOT.EnableImplicitMT
 def main():
     # Names within ttree
     treeFolder = "analysis/allEvents/"
-    treeName = "massData"
 
-    weightBranch =  "weight"
+    mumujjNtupleName = "bg_mumujjRecoMass"
+    mumujjEventWeightBranch = "eventWeight"
 
+    eejjNtupleName = "bg_eejjRecoMass"
+    eejjEventWeightBranch = "eventWeight"
 
     #LOADING THE TTREE
     fileNames = [ "DY100to200.root",
@@ -31,24 +33,33 @@ def main():
     # weight related
     crossSections = [147.4,  40.99, 5.678, 2.198, 0.6304, 0.1514, 0.003565, 88.29]
     counts2 = [2751187, 962195, 1070454, 8292957, 2673066, 596079, 399492, 79140880]
-    lumi = 137 #/fb
+    lumi = 137000 #/pb
 
-    sampleNum = 0.0
+    sampleNum_mumujj = 0.0
+    sampleNum_eejj = 0.0
+
     for fileName, xSec, count2 in zip(fileNames, crossSections, counts2):
     	rootfile= ROOT.TFile.Open(fileName, "read")
 
-    	massTree = rootfile.Get(treeFolder+treeName)
+    	mumujjNtuple = rootfile.Get(analysisFolder+mumujjNtupleName)
+        eejjNtuple = rootfile.Get(analysisFolder+eejjNtupleName)
 
-    	weightArray = tree2array(massTree, branches=weightBranch)
+        mumujjEventWeightArray = tree2array(mumujjNtuple, branches=mumujjEventWeightBranch)
+
+        eejjEventWeightArray = tree2array(eejjNtuple, branches=eejjEventWeightBranch)
 
         allInteractionNum = xSec*lumi
-        recoRatio = weightArray.sum()/count2
-        print allInteractionNum, recoRatio, recoRatio*allInteractionNum
-        sampleNum += recoRatio*allInteractionNum
+        mumujjRecoRatio = mumujjEventWeightArray.sum()/count2
+        eejjRecoRatio = eejjEventWeightArray.sum()/count2
 
-    sampleNum = int(sampleNum)
+        sampleNum_mumujj += allInteractionNum*mumujjRecoRatio
+        sampleNum_eejj += allInteractionNum*eejjRecoRatio
 
-    print("You should generate",sampleNum,"samples")
+    sampleNum_mumujj = int(sampleNum_mumujj)
+    sampleNum_eejj = int(sampleNum_eejj)
+
+    print "You should generate",sampleNum_mumujj,"samples for eejj"
+    print "You should generate",sampleNum_eejj,"samples for mumujj"
 
 if(__name__=="__main__"):
     main()
