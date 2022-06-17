cf0384c43abe9a32315825b9cd16fa2ad3418cfd
Billy Li <li000400@umn.edu>
2022-06-16 23:30:37 -0500

    preparing roofit MC generation

---
+++ roofit/experiments/ratio_estimation/gen/genWR1600N800.C
@@ -0,0 +1,23 @@
+#include "RooRealVar.h"
+#include "RooDataSet.h"
+#include "RooDataHist.h"
+#include "RooGaussian.h"
+#include "TCanvas.h"
+#include "RooPlot.h"
+#include "TTree.h"
+#include "TH1D.h"
+#include "TRandom.h"
+#include "RooExpmCB.h"
+using namespace RooFit;
+
+void genWR1600N800()
+{
+  // init WR distribution
+
+  // init bg distribution
+
+  // add distribution
+
+  // generate sample
+  return
+}
