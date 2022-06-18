adac4fdbdf16e5dc3b20c044963e035fd10201a1
Billy Li <li000400@umn.edu>
2022-06-17 22:43:35 -0500

    try

--- roofit/testFit_bg_Expm.C
+++ roofit/testFit_bg_Expm.C
@@ -101,8 +101,8 @@ void testFit_bg_Expm()
   RooRealVar *b_mm = new RooRealVar("b_mm", "b_mm", 5.16e-01, 4.8e-1, 5.5e-1);
   RooExpm *model_mm = new RooExpm("mumujj bg from DY+ttbar ", "exponential bg", *mumujjMass_bg, *a_mm, *b_mm);
   // eejj
-  RooRealVar *a_ee = new RooRealVar("a_ee", "a_ee", -1e-01, -6e-1, -1e-2);
-  RooRealVar *b_ee = new RooRealVar("b_ee", "b_ee", 1e-01, 1e-2, 9e-1);
+  RooRealVar *a_ee = new RooRealVar("a_ee", "a_ee", -2.4e-01, -30e-1, -1e-2);
+  RooRealVar *b_ee = new RooRealVar("b_ee", "b_ee", 5e-01, 1e-2, 20e-1);
   RooExpm *model_ee = new RooExpm("eejj bg from DY+ttbar ", "exponential bg", *eejjMass_bg, *a_ee, *b_ee);
 
   //// fit model
