79c0eb61f6c79115361b2df0951328b4cfa3c0c9
Billy Li <li000400@umn.edu>
2022-06-20 16:30:47 -0500

    dbeug

--- roofit/experiments/ratio_estimation/fit/fitRatio_WR1600N800.C
+++ roofit/experiments/ratio_estimation/fit/fitRatio_WR1600N800.C
@@ -145,7 +145,7 @@ void fitRatio_WR1600N800()
     RooFitResult *r_mumu = model_mumu->fitTo(ds_mumujj, Save(), SumW2Error(kTRUE), Range(800,2000));
     RooFitResult *r_ee= model_ee->fitTo(ds_eejj, Save(), SumW2Error(kTRUE), Range(800,2000));
 
-    f->cd();
+    f.cd();
     r_mumu->Write("mumu");
     r_ee->Write("ee");
   }
