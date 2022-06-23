13e886898d4fd8a0ab9a06e2f1c4e3172c9c70d6
Billy Li <li000400@umn.edu>
2022-06-23 17:07:33 -0500

    change initialization

--- roofit/testFit_bg_Expm.C
+++ roofit/testFit_bg_Expm.C
@@ -97,12 +97,12 @@ void testFit_bg_Expm()
 
   //// declare model
   // mumujj
-  RooRealVar *a_mm = new RooRealVar("a_mm", "a_mm", -1.9952e-01, -2.0e-1, -1.7e-1);
-  RooRealVar *b_mm = new RooRealVar("b_mm", "b_mm", 5.16e-01, 4.8e-1, 5.5e-1);
+  RooRealVar *a_mm = new RooRealVar("a_mm", "a_mm", -2e-1, -6e-1, -5e-2);
+  RooRealVar *b_mm = new RooRealVar("b_mm", "b_mm", 5e-01, 2e-1, 8e-1);
   RooExpm *model_mm = new RooExpm("mumujj bg from DY+ttbar ", "exponential bg", *mumujjMass_bg, *a_mm, *b_mm);
   // eejj
-  RooRealVar *a_ee = new RooRealVar("a_ee", "a_ee", -2.2e-01, -2.6e-1, -1e-1);
-  RooRealVar *b_ee = new RooRealVar("b_ee", "b_ee", 6e-01, 1e-2, 6e-1);
+  RooRealVar *a_ee = new RooRealVar("a_ee", "a_ee", -2e-01, -6e-1, -5e-2);
+  RooRealVar *b_ee = new RooRealVar("b_ee", "b_ee", 5e-01, 2e-2, 8e-1);
   RooExpm *model_ee = new RooExpm("eejj bg from DY+ttbar ", "exponential bg", *eejjMass_bg, *a_ee, *b_ee);
 
   //// fit model
