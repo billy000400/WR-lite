e2f7d869b1ea6c95527b120753fbd3d2ffd68d00
Billy Li <li000400@umn.edu>
2022-06-24 15:53:43 -0500

    go!

--- roofit/experiments/ratio_estimation/fitRatio/1e-2/fitRatio_WR1600N800.C
+++ roofit/experiments/ratio_estimation/fitRatio/1e-2/fitRatio_WR1600N800.C
@@ -31,7 +31,7 @@ void fitRatio_WR1600N800()
 
   //// set sample number
   int init = 0;
-  int sampleNum = 3;
+  int sampleNum = 1000;
   // int mumujjEventNum = 515750;
   // int eejjEventNum = 727838;
 
