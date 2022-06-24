b7f279cb11e723e77667067c5eb503b7a9b7cef3
Billy Li <li000400@umn.edu>
2022-06-23 19:57:30 -0500

    uncomment lines

--- misc/extractRootFiles.sh
+++ misc/extractRootFiles.sh
@@ -2,7 +2,7 @@
 # @Date:   05-03-2022
 # @Email:  li000400@umn.edu
 # @Last modified by:   billyli
-# @Last modified time: 05-05-2022
+# @Last modified time: 06-23-2022
 
 
 
@@ -44,181 +44,181 @@ cd ..
 scp -r DY*/DY*.root analysis/allEvents
 scp -r TT*/TT*.root analysis/allEvents
 
-# 
-#
-# cd /data/cmszfs1/user/li000400/CMSSW_10_4_0_patch1/src/ExoAnalysis/WR_lite/WR1000_N200
-# rm WR1000N200.root
-# hadd WR1000N200.root *.root
-#
-# cd ../WR1000_N400
-# rm WR1000N400.root
-# hadd WR1000N400.root *.root
-#
-# cd ../WR1000_N600
-# rm WR1000N600.root
-# hadd WR1000N600.root *.root
-#
-# cd ../WR1000_N800
-# rm WR1000N800.root
-# hadd WR1000N800.root *.root
-#
-# cd ../WR1000_N900
-# rm WR1000N900.root
-# hadd WR1000N900.root *.root
-#
-# cd ../WR1200_N1000
-# rm WR1200N1000.root
-# hadd WR1200N1000.root *.root
-#
-# cd ../WR1200_N1100
-# rm WR1200N1100.root
-# hadd WR1200N1100.root *.root
-#
-# cd ../WR1200_N400
-# rm WR1200N400.root
-# hadd WR1200N400.root *.root
-#
-# cd ../WR1200_N600
-# rm WR1200N600.root
-# hadd WR1200N600.root *.root
-#
-# cd ../WR1200_N800
-# rm WR1200N800.root
-# hadd WR1200N800.root *.root
-#
-# cd ../WR1400_N1000
-# rm WR1400N1000.root
-# hadd WR1400N1000.root *.root
-#
-# cd ../WR1400_N1200
-# rm WR1400N1200.root
-# hadd WR1400N1200.root *.root
-#
-# cd ../WR1400_N1300
-# rm WR1400N1300.root
-# hadd WR1400N1300.root *.root
-#
-# cd ../WR1400_N400
-# rm WR1400N400.root
-# hadd WR1400N400.root *.root
-#
-# cd ../WR1400_N600
-# rm WR1400N600.root
-# hadd WR1400N600.root *.root
-#
-# cd ../WR1400_N800
-# rm WR1400N800.root
-# hadd WR1400N800.root *.root
-#
-# cd ../WR1600_N1000
-# rm WR1600N1000.root
-# hadd WR1600N1000.root *.root
-#
-# cd ../WR1600_N1200
-# rm WR1600N1200.root
-# hadd WR1600N1200.root *.root
-#
-# cd ../WR1600_N1400
-# rm WR1600N1400.root
-# hadd WR1600N1400.root *.root
-#
-# cd ../WR1600_N1500
-# rm WR1600N1500.root
-# hadd WR1600N1500.root *.root
-#
-# cd ../WR1600_N400
-# rm WR1600N400.root
-# hadd WR1600N400.root *.root
-#
-# cd ../WR1600_N600
-# rm WR1600N600.root
-# hadd WR1600N600.root *.root
-#
-# cd ../WR1600_N800
-# rm WR1600N800.root
-# hadd WR1600N800.root *.root
-#
-# cd ../WR1800_N1000
-# rm WR1800N1000.root
-# hadd WR1800N1000.root *.root
-#
-# cd ../WR1800_N1200
-# rm WR1800N1200.root
-# hadd WR1800N1200.root *.root
-#
-# cd ../WR1800_N1400
-# rm WR1800N1400.root
-# hadd WR1800N1400.root *.root
-#
-# cd ../WR1800_N1600
-# rm WR1800N1600.root
-# hadd WR1800N1600.root *.root
-#
-# cd ../WR1800_N1700
-# rm WR1800N1700.root
-# hadd WR1800N1700.root *.root
-#
-# cd ../WR1800_N400
-# rm WR1800N400.root
-# hadd WR1800N400.root *.root
-#
-# cd ../WR1800_N600
-# rm WR1800N600.root
-# hadd WR1800N600.root *.root
-#
-# cd ../WR1800_N800
-# rm WR1800N800.root
-# hadd WR1800N800.root *.root
-#
-# cd ../WR2000_N1000
-# rm WR2000N1000.root
-# hadd WR2000N1000.root *.root
-#
-# cd ../WR2000_N1200
-# rm WR2000N1200.root
-# hadd WR2000N1200.root *.root
-#
-# cd ../WR2000_N1400
-# rm WR2000N1400.root
-# hadd WR2000N1400.root *.root
-#
-# cd ../WR2000_N1600
-# rm WR2000N1600.root
-# hadd WR2000N1600.root *.root
-#
-# cd ../WR2000_N1800
-# rm WR2000N1800.root
-# hadd WR2000N1800.root *.root
-#
-# cd ../WR2000_N1900
-# rm WR2000N1900.root
-# hadd WR2000N1900.root *.root
-#
-# cd ../WR2000_N600
-# rm WR2000N600.root
-# hadd WR2000N600.root *.root
-#
-# cd ../WR2000_N800
-# rm WR2000N800.root
-# hadd WR2000N800.root *.root
-#
-# cd ../WR800_N200
-# rm WR800N200.root
-# hadd WR800N200.root *.root
-#
-# cd ../WR800_N400
-# rm WR800N400.root
-# hadd WR800N400.root *.root
-#
-# cd ../WR800_N600
-# rm WR800N600.root
-# hadd WR800N600.root *.root
-#
-# cd ../WR800_N700
-# rm WR800N700.root
-# hadd WR800N700.root *.root
-#
-# cd ..
-#
-#
-# scp -r WR*/WR*.root analysis/allEvents
+
+
+cd /data/cmszfs1/user/li000400/CMSSW_10_4_0_patch1/src/ExoAnalysis/WR_lite/WR1000_N200
+rm WR1000N200.root
+hadd WR1000N200.root *.root
+
+cd ../WR1000_N400
+rm WR1000N400.root
+hadd WR1000N400.root *.root
+
+cd ../WR1000_N600
+rm WR1000N600.root
+hadd WR1000N600.root *.root
+
+cd ../WR1000_N800
+rm WR1000N800.root
+hadd WR1000N800.root *.root
+
+cd ../WR1000_N900
+rm WR1000N900.root
+hadd WR1000N900.root *.root
+
+cd ../WR1200_N1000
+rm WR1200N1000.root
+hadd WR1200N1000.root *.root
+
+cd ../WR1200_N1100
+rm WR1200N1100.root
+hadd WR1200N1100.root *.root
+
+cd ../WR1200_N400
+rm WR1200N400.root
+hadd WR1200N400.root *.root
+
+cd ../WR1200_N600
+rm WR1200N600.root
+hadd WR1200N600.root *.root
+
+cd ../WR1200_N800
+rm WR1200N800.root
+hadd WR1200N800.root *.root
+
+cd ../WR1400_N1000
+rm WR1400N1000.root
+hadd WR1400N1000.root *.root
+
+cd ../WR1400_N1200
+rm WR1400N1200.root
+hadd WR1400N1200.root *.root
+
+cd ../WR1400_N1300
+rm WR1400N1300.root
+hadd WR1400N1300.root *.root
+
+cd ../WR1400_N400
+rm WR1400N400.root
+hadd WR1400N400.root *.root
+
+cd ../WR1400_N600
+rm WR1400N600.root
+hadd WR1400N600.root *.root
+
+cd ../WR1400_N800
+rm WR1400N800.root
+hadd WR1400N800.root *.root
+
+cd ../WR1600_N1000
+rm WR1600N1000.root
+hadd WR1600N1000.root *.root
+
+cd ../WR1600_N1200
+rm WR1600N1200.root
+hadd WR1600N1200.root *.root
+
+cd ../WR1600_N1400
+rm WR1600N1400.root
+hadd WR1600N1400.root *.root
+
+cd ../WR1600_N1500
+rm WR1600N1500.root
+hadd WR1600N1500.root *.root
+
+cd ../WR1600_N400
+rm WR1600N400.root
+hadd WR1600N400.root *.root
+
+cd ../WR1600_N600
+rm WR1600N600.root
+hadd WR1600N600.root *.root
+
+cd ../WR1600_N800
+rm WR1600N800.root
+hadd WR1600N800.root *.root
+
+cd ../WR1800_N1000
+rm WR1800N1000.root
+hadd WR1800N1000.root *.root
+
+cd ../WR1800_N1200
+rm WR1800N1200.root
+hadd WR1800N1200.root *.root
+
+cd ../WR1800_N1400
+rm WR1800N1400.root
+hadd WR1800N1400.root *.root
+
+cd ../WR1800_N1600
+rm WR1800N1600.root
+hadd WR1800N1600.root *.root
+
+cd ../WR1800_N1700
+rm WR1800N1700.root
+hadd WR1800N1700.root *.root
+
+cd ../WR1800_N400
+rm WR1800N400.root
+hadd WR1800N400.root *.root
+
+cd ../WR1800_N600
+rm WR1800N600.root
+hadd WR1800N600.root *.root
+
+cd ../WR1800_N800
+rm WR1800N800.root
+hadd WR1800N800.root *.root
+
+cd ../WR2000_N1000
+rm WR2000N1000.root
+hadd WR2000N1000.root *.root
+
+cd ../WR2000_N1200
+rm WR2000N1200.root
+hadd WR2000N1200.root *.root
+
+cd ../WR2000_N1400
+rm WR2000N1400.root
+hadd WR2000N1400.root *.root
+
+cd ../WR2000_N1600
+rm WR2000N1600.root
+hadd WR2000N1600.root *.root
+
+cd ../WR2000_N1800
+rm WR2000N1800.root
+hadd WR2000N1800.root *.root
+
+cd ../WR2000_N1900
+rm WR2000N1900.root
+hadd WR2000N1900.root *.root
+
+cd ../WR2000_N600
+rm WR2000N600.root
+hadd WR2000N600.root *.root
+
+cd ../WR2000_N800
+rm WR2000N800.root
+hadd WR2000N800.root *.root
+
+cd ../WR800_N200
+rm WR800N200.root
+hadd WR800N200.root *.root
+
+cd ../WR800_N400
+rm WR800N400.root
+hadd WR800N400.root *.root
+
+cd ../WR800_N600
+rm WR800N600.root
+hadd WR800N600.root *.root
+
+cd ../WR800_N700
+rm WR800N700.root
+hadd WR800N700.root *.root
+
+cd ..
+
+
+scp -r WR*/WR*.root analysis/allEvents
