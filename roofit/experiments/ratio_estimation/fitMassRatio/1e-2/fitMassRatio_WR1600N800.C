99e569421f8cbbd0315416ccc21ed7f0cd02a4de
Billy Li <li000400@umn.edu>
2022-06-27 12:56:01 -0500

    test

--- roofit/experiments/ratio_estimation/fitMassRatio/1e-2/fitMassRatio_WR1600N800.C
+++ roofit/experiments/ratio_estimation/fitMassRatio/1e-2/fitMassRatio_WR1600N800.C
@@ -30,8 +30,8 @@ void fitMassRatio_WR1600N800()
   // RooFit::RooMsgService::instance().getStream(1).removeTopic(NumericIntegration) ;
 
   //// set sample number
-  int init = 0;
-  int sampleNum = 1000;
+  int init = 66;
+  int sampleNum = 2;
   // int mumujjEventNum = 515750;
   // int eejjEventNum = 727838;
 
