49d89284c83a1b231c53cd06dcfc75a4d87b02b3
Billy Li <li000400@umn.edu>
2022-06-29 15:14:31 -0500

    dev

--- roofit/experiments/ratio_estimation/fitMassRatio/fit_1e-2.sh
+++ roofit/experiments/ratio_estimation/fitMassRatio/fit_1e-2.sh
@@ -4,14 +4,18 @@
 # @Last modified by:   billyli
 # @Last modified time: 06-29-2022
 
-sampleFileDir="../../../data/RooFitMC/ratio_1e-2/WR1600N800/"
-declare -i i=1
-for f in $sampleFileDir/*.root; do
-  filename=$(basename $f)
-  root -l -b -q RooExpm.cxx RooExpmCB.cxx "fitMassRatio.C(\"$filename\",\"1e-2\")" > /dev/null &
-  echo "processing $filename"
-  if (( $i%8==0 )); then
-      wait
-  fi
-  i=$(($i + 1))
-done
+# sampleFileDir="../../../data/RooFitMC/ratio_1e-2/WR1600N800/"
+# declare -i i=1
+# for f in $sampleFileDir/*.root; do
+#   filename=$(basename $f)
+#   root -l -b -q RooExpm.cxx RooExpmCB.cxx "fitMassRatio.C(\"$filename\",\"1e-2\")" > /dev/null &
+#   echo "processing $filename"
+#   if (( $i%8==0 )); then
+#       wait
+#   fi
+#   i=$(($i + 1))
+# done
+
+cd ../../../data/MCFitResult/fitMassRatio/ratio_1e-2
+rm MCFitResult_WR1600N800.root
+hadd MCFitResult_WR1600N800.root *.root
