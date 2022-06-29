37c8abb69250eb42b4b68705088e1ca50d7925ef
Billy Li <li000400@umn.edu>
2022-06-29 12:48:34 -0500

    test

---
+++ roofit/experiments/ratio_estimation/fitMassRatio/fit_1e-2.sh
@@ -0,0 +1,16 @@
+# @Author: Billy Li <billyli>
+# @Date:   06-29-2022
+# @Email:  li000400@umn.edu
+# @Last modified by:   billyli
+# @Last modified time: 06-29-2022
+
+sampleFileDir="../../../data/RooFitMC/ratio_1e-2/WR1600N800/"
+declare -i i=1
+for f in $sampleFileDir/*.root; do
+  filename=$(basename $f)
+  root -l -b -q RooExpm.cxx RooExpmCB.cxx "fitMassRatio.C(\"$filename\",\"1e-2\")"
+  if (( $i%7==0 )); then
+      wait
+  fi
+  i = i+1
+done
