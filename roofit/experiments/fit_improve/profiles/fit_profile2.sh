c91509290757624107da7da3f04ba5ba1b1e8b43
Billy Li <li000400@umn.edu>
2022-06-29 13:03:48 -0500

    dev

--- roofit/experiments/fit_improve/profiles/fit_profile2.sh
+++ roofit/experiments/fit_improve/profiles/fit_profile2.sh
@@ -3,10 +3,11 @@
 # @Date:   05-19-2022
 # @Email:  li000400@umn.edu
 # @Last modified by:   billyli
-# @Last modified time: 05-19-2022
+# @Last modified time: 06-29-2022
 
 fullFileDir="../../../../analysis/allEvents"
 for i in $fullFileDir/fullWR*.root; do
   filename=$(basename $i)
+  echo "processing $filename"
   root -l -b RooExpmCB.cxx "profile2.C(\"$filename\")" -q
 done
