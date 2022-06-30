df009b5704bfc6c8da89d89bd936b6553bceaf90
Billy Li <li000400@umn.edu>
2022-06-29 20:25:55 -0500

    dev

--- roofit/experiments/fit_gen/RooJohnson.cxx
+++ roofit/experiments/fit_gen/RooJohnson.cxx
@@ -112,13 +112,6 @@ double RooJohnson::evaluate() const
 
 ////////////////////////////////////////////////////////////////////////////////
 /// Compute multiple values of the Johnson distribution.
-void RooJohnson::computeBatch(cudaStream_t* stream, double* output, size_t nEvents, RooFit::Detail::DataMap const& dataMap) const
-{
-  auto dispatch = stream ? RooBatchCompute::dispatchCUDA : RooBatchCompute::dispatchCPU;
-  dispatch->compute(stream, RooBatchCompute::Johnson, output, nEvents,
-          {dataMap.at(_mass), dataMap.at(_mu), dataMap.at(_lambda), dataMap.at(_gamma), dataMap.at(_delta)},
-          {_massThreshold});
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 
